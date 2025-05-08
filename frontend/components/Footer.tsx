216fa57faf6b5711fbb59f70fc5a297b7cd32349
michaelmarsillo <thecaptain49@hotmail.ca>
2025-05-08 12:29:18 -0400

    changed footer component

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className="flex items-center justify-center py-4 bg-black/20 text-gray-400 mt-20">
       <p className="text-sm text-center">
         Made with <span className="text-red-500">💖</span> by 
-        <a href="https://github.com/michaelmarsillo" className="text-blue-500 hover:underline ml-1 mr-1">
+        <a href="https://github.com/michaelmarsillo" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline ml-1 mr-1">
           Michael Marsillo
         </a> 
          &copy; 2025 all rights reserved.
