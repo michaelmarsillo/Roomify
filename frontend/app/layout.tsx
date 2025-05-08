4d380e0ec661d5f0586b31ddf23a796136992225
michaelmarsillo <thecaptain49@hotmail.ca>
2025-05-07 22:38:49 -0400

    changed web app title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "TFSA Calculator",
+  title: "Roomify",
   description: "Track your TFSA contributions and withdrawals",
 };
 
